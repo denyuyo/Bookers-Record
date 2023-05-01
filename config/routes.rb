98a7d5b934d74d596df4eb99b37462ddb8f0a6b8
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-05-01 11:08:47 +0000

    Asynchronous Favorite

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  devise_for :users
+  root to: "homes#top"
+  get "/home/about"=>"homes#about"
+  get '/search', to: 'searchs#search'
+  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create]
+  end
+  resources :book_comments, only: [:destroy]
+end
